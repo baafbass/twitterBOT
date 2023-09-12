fe29e46adacf3c35792aa264083810c04dded217
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-12 12:55:55 +0300

    My First Twitter Bot 2

--- tweet.py
+++ tweet.py
@@ -1,14 +1,21 @@
 import tweepy
+import time
 
 auth = tweepy.OAuthHandler('es1YvJmHHUsxB9y9eyxLeDB0R','tDyAkniVzcuyfung0PLWqJIUlG2JKj9arzEi4lY39v0tlctVTu')
 auth.set_access_token('1302250654535221251-UmMSwLneYzMLSldeiBba3AQOlkJdFb', 'aCnzx2PzIvXACG0zHmHoO3rvqnI3Z10R2H1Hy4iX0YDjY')
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline() 
-for tweet in public_tweets:
-    print(tweet.text) # Will print the tweets from our home_timelines
-
 user = api.me() # Get my twitter profile
-print(user.name) # To get our profile name
-print(user.follwers_count)
+
+def limit_handler(cursor):
+	try:
+	     while True:
+		     yield cursor.next()
+	except tweepy.RateLimitError:
+		time.sleep(1000)
+
+for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+	print(follower.name) # will print all the my followers
+	if follower.name == 'Nabila Bassirou':
+		follower.follow() # Will follow 'Nabila' From my followers
